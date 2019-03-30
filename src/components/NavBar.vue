5873fd3a576de9fe15e297038b46d670ab68060d
Ashiri <ajaoiyanu@gmail.com>
2019-03-30 21:49:35 +0100

    Create NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a>Real Time Docs</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
+  </nav>
+
+</template>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+
+</style>
