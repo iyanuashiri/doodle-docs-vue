1aaa0386e33b600e3f8d52731abc13d7acab02b1
Ashiri <ajaoiyanu@gmail.com>
2019-04-01 16:14:42 +0100

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!--<div id="nav">-->
+      <!--&lt;!&ndash;<router-link to="/">Home</router-link> |&ndash;&gt;-->
+      <!--&lt;!&ndash;<router-link to="/about">About</router-link>&ndash;&gt;-->
+    <!--</div>-->
     <router-view/>
   </div>
 </template>
@@ -16,16 +16,16 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
+/*#nav {*/
+  /*padding: 30px;*/
+/*}*/
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+/*#nav a {*/
+  /*font-weight: bold;*/
+  /*color: #2c3e50;*/
+/*}*/
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/*#nav a.router-link-exact-active {*/
+  /*color: #42b983;*/
+/*}*/
 </style>
