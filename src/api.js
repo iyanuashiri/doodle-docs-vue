6198da0593fbd304da997bb45821c4cbf3b35a84
Ashiri <ajaoiyanu@gmail.com>
2019-04-01 16:11:18 +0100

    Update api functions

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://127.0.0.1:8000/api/v2/',
   json: true
 })
 
 export default {
   async execute (method, resource, data) {
-    let accessToken = await Vue.prototype.$auth.getAccessToken()
+    // let accessToken = await Vue.prototype.$auth.getAccessToken()
     return client({
       method,
       url: resource,
-      data,
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
+      data
+      // headers: {
+      //   Authorization: `Bearer ${accessToken}`
+      // }
     }).then(req => {
       return req.data
     })
@@ -27,7 +27,7 @@ export default {
     return this.execute('get', `/docs/${id}`)
   },
   createDoc (data) {
-    return this.execute('post', '/docs')
+    return this.execute('post', '/docs', data)
   },
   updateDoc (id, data) {
     return this.execute('put', `/docs/${id}`, data)
