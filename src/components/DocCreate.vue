dd9f8ec4c7cd4ada96b187958451fb6de6a91fe3
Ashiri <ajaoiyanu@gmail.com>
2019-04-02 19:00:55 +0100

    Add create

---
+++ src/components/DocCreate.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="doc-create">
+    <Navbar></Navbar>
+    <h1>{{ page_title }}</h1>
+
+    <form v-on:submit.prevent="saveDoc" class="form-example">
+      <div class="form-example">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" v-model="model.title" required>
+      </div>
+      <div class="form-example">
+        <label for="body">Body</label>
+        <input type="text" name="body" id="body" v-model="model.body" required>
+      </div>
+      <div class="form-example">
+        <button type="submit">Save Doc</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+import Navbar from '@/components/NavBar.vue'
+
+export default {
+  name: 'doc-create',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+      page_title: 'Create Doc',
+      model: {}
+    }
+  },
+  methods: {
+    saveDoc: async function () {
+      await api.createDoc(this.model)
+      await this.$router.push({ name: 'doc-detail', params: { id: this.model.id } })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  form.form-example {
+    display: table;
+  }
+
+  div.form-example {
+    display: table-row;
+  }
+
+  label, input {
+    display: table-cell;
+    margin-bottom: 10px;
+  }
+
+  label {
+    padding-right: 10px;
+  }
+</style>
